4f4fc475c9e22d9c0da17233b5194caeb6c3e2f7
eunga <50609486+yerin1211@users.noreply.github.com>
2020-06-10 14:32:30 +0900

    Create stage.h

---
+++ stage.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Main.h"
+#include "Utility.h"
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <mmsystem.h> 
+#include <stdlib.h>
+#include <time.h>
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "msimg32.lib")
+
+
+void round0();
+void round1();
+void round2();
+void round3();
+void gameclear();
